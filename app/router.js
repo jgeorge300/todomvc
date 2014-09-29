16aea70f3339561d350ff38335b7db47edbd14b4
George, Joseph <joseph.george@cardinalhealth.com>
2014-09-28 20:42:22 -0400

    Adding Child routes

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+  	
+  });
 });
 
 export default Router;
